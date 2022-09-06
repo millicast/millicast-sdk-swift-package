1482c267b4016147ba934330050c8e31d1bc4682
Santiago-Souto <santiagos@qualabs.com>
2022-09-06 19:15:54 -0300

    Update to 1.3.1

--- Package.swift
+++ Package.swift
@@ -23,8 +23,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "MillicastSDK",
-            url: "https://github.com/millicast/millicast-native-sdk/releases/download/v1.3.0/millicast-native-sdk-1.3.0-XCFramework.zip",
-            checksum: "ca725b2359bb8b6f15b4a8facea9a2c6342fa2dc67839247edaf582d37c7edf3"
+            url: "https://github.com/millicast/millicast-native-sdk/releases/download/v1.3.1/millicast-native-sdk-1.3.1-XCFramework.zip",
+            checksum: "a6146d244de62e82f6e85b8dac1e4506cc182d1a69e0c879be474f004a101288"
         )
     ]
 )
